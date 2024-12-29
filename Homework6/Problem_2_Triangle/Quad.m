9237c35b4570f27fc02754fc2084dee0752e0db0
BKS <12110908@mail.sustech.edu.cn>
2024-12-29 19:44:52 +0800

    Finish the Problem2_a by change the mesh to triangle element and modify Quad and Quad_grad for Na shape function

--- Homework6/Problem_2_Triangle/Quad.m
+++ Homework6/Problem_2_Triangle/Quad.m
@@ -1,15 +1,27 @@
 function val = Quad(aa, xi, eta)
 
+% Triangle element
 if aa == 1
-    val = 0.25 * (1-xi) * (1-eta);
+    val = 1 - xi - eta;
 elseif aa == 2
-    val = 0.25 * (1+xi) * (1-eta);
+    val = xi;
 elseif aa == 3
-    val = 0.25 * (1+xi) * (1+eta);
-elseif aa == 4
-    val = 0.25 * (1-xi) * (1+eta);
+    val = eta;
 else
-    error('Error: value of a should be 1,2,3, or 4.');
+    error('Error: value of a should be 1,2,3');
 end
+
+% Quadrilateral element
+% if aa == 1
+%     val = 0.25 * (1-xi) * (1-eta);
+% elseif aa == 2
+%     val = 0.25 * (1+xi) * (1-eta);
+% elseif aa == 3
+%     val = 0.25 * (1+xi) * (1+eta);
+% elseif aa == 4
+%     val = 0.25 * (1-xi) * (1+eta);
+% else
+%     error('Error: value of a should be 1,2,3, or 4.');
+% end
 
 % EOF
