a5e95b38f72bafa2d2d67e07b12af87142807790
BKS <12110908@mail.sustech.edu.cn>
2024-12-29 22:57:38 +0800

    Finishe error calculation for quadrilateral element

--- Homework6/Problem_2_Quadrilateral/driver.m
+++ Homework6/Problem_2_Quadrilateral/driver.m
@@ -15,10 +15,16 @@ n_int_eta = 3;
 n_int     = n_int_xi * n_int_eta;
 [xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);
 
+lne0_table = zeros(1,7);
+lne1_table = zeros(1,7);
+lnh_table  = zeros(1,7);
+
+for hh = 30:30:210
+
 % mesh generation
 n_en   = 4;               % number of nodes in an element
-n_el_x = 60;               % number of elements in x-dir
-n_el_y = 60;               % number of elements in y-dir
+n_el_x = hh;               % number of elements in x-dir
+n_el_y = hh;               % number of elements in y-dir
 n_el   = n_el_x * n_el_y; % total number of elements
 
 n_np_x = n_el_x + 1;      % number of nodal points in x-dir
@@ -148,8 +154,79 @@ for ii = 1 : n_np
   end
 end
 
+% Error calculation
+e0_inner = 0;
+e1_inner = 0;
+
+for ee = 1 : n_el
+    x_ele = x_coor(IEN(ee,:));
+    y_ele = y_coor(IEN(ee,:));
+    u_ele = disp(IEN(ee,:));
+    for ll = 1 : n_int
+        x_l = 0.0; y_l = 0.0;
+        dx_dxi = 0.0; dx_deta = 0.0;
+        dy_dxi = 0.0; dy_deta = 0.0;
+        uh = 0.0;
+        uh_xi  = 0.0; uh_eta  = 0.0;
+        for aa = 1 : n_en
+            x_l = x_l + x_ele(aa) * Quad(aa, xi(ll), eta(ll));
+            y_l = y_l + y_ele(aa) * Quad(aa, xi(ll), eta(ll));
+            uh  = uh  + u_ele(aa) * Quad(aa, xi(ll), eta(ll));
+            [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
+            dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
+            dx_deta = dx_deta + x_ele(aa) * Na_eta;
+            dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
+            dy_deta = dy_deta + y_ele(aa) * Na_eta;
+        end
+        dxi_dx = 1/dx_dxi;
+        deta_dy= 1/dy_deta;
+        detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
+
+        for aa = 1 : n_en
+            Na = Quad(aa, xi(ll), eta(ll));
+            [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
+            Na_x = (Na_xi * dy_deta - Na_eta * dy_dxi) / detJ;
+            Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
+            uh_xi = uh_xi + u_ele(aa) * Na_xi;
+            uh_eta = uh_eta + u_ele(aa) * Na_eta;
+        end
+        
+        e0_inner = e0_inner + weight(ll) * (uh - exact(x_l,y_l))^2 * detJ;
+        e1_inner = e1_inner + weight(ll) * ((uh - exact(x_l,y_l))^2 + (uh_xi * dxi_dx - exact_x(x_l,y_l))^2 + (uh_eta * deta_dy - exact_y(x_l,y_l))^2) * detJ;
+
+    end
+end
+e0 = sqrt(e0_inner);
+e1 = sqrt(e1_inner);
+
+lne0 = log(e0);
+lne1 = log(e1);
+lnh = log(hh);
+lne0_table(hh/30) = lne0;
+lne1_table(hh/30) = lne1;
+lnh_table(hh/30) = lnh;
+
+end
+
+f1 = figure;
+plot(lnh_table,lne0_table);
+hold on
+plot(lnh_table,lne1_table);
+legend('e0','e1','Location','southeast')
+xlabel('ln(h)')
+ylabel('ln(e)')
+title('log-log plot of Error versus mesh size')
+p_e0 = polyfit(lnh_table,lne0_table,1);
+p_e1 = polyfit(lnh_table,lne1_table,1);
+slope_e0 = p_e0(1)
+slope_e1 = p_e1(1)
+
+
+
+
+%%
 % save the solution vector and number of elements to disp with name
 % HEAT.mat
-save("HEAT", "disp", "n_el_x", "n_el_y");
+% save("HEAT", "disp", "n_el_x", "n_el_y");
 
 % EOF
