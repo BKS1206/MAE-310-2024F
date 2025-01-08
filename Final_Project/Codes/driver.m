b67fc0ccf36a8f4bbc3c65935a99392a13d96f8d
BKS <12110908@mail.sustech.edu.cn>
2025-01-08 21:25:24 +0800

    Add gauss quadrature rule and shape function for quadraliteralelements

--- Final_Project/Codes/driver.m
+++ Final_Project/Codes/driver.m
@@ -39,79 +39,114 @@ clear;clc; close all;
     fclose(fid);
 
 %% Read nodes and IEN for case 2
-    % 打开文件
-    fid = fopen('quarter-plate-with-hole-quad-Refine4.msh', 'r');
-
-    if fid == -1
-        error('无法打开文件');
-    end
-    
-    % 初始化
-    i = 1;
-    j = 1;
-    coor = zeros(1,3);
-    ien  = zeros(1,5);
-    % 读取文件内容
-   while ~feof(fid)
-        line = fgetl(fid);
-        num  = strsplit(line);
-        num = str2double(num(1,:));
-        Y = isnan(num);
-        num = num (~Y);
-        isdata = all(isnan(num));
-        if isdata == 0
-            if length(num) == 3
-                coor(i,:) = num;
-                i = i+1;
-            end
-            if length(num) == 5
-                ien(j,:) = num;
-                j = j+1;
-            end
-        end
-   end
-    node_coor_2 = coor(2:2147,:);
-    IEN_2 = ien(8:2055,2:5);
-    % 关闭文件
-    fclose(fid);
+   %  % 打开文件
+   %  fid = fopen('quarter-plate-with-hole-quad-Refine4.msh', 'r');
+   % 
+   %  if fid == -1
+   %      error('无法打开文件');
+   %  end
+   % 
+   %  % 初始化
+   %  i = 1;
+   %  j = 1;
+   %  coor = zeros(1,3);
+   %  ien  = zeros(1,5);
+   %  % 读取文件内容
+   % while ~feof(fid)
+   %      line = fgetl(fid);
+   %      num  = strsplit(line);
+   %      num = str2double(num(1,:));
+   %      Y = isnan(num);
+   %      num = num (~Y);
+   %      isdata = all(isnan(num));
+   %      if isdata == 0
+   %          if length(num) == 3
+   %              coor(i,:) = num;
+   %              i = i+1;
+   %          end
+   %          if length(num) == 5
+   %              ien(j,:) = num;
+   %              j = j+1;
+   %          end
+   %      end
+   % end
+   %  node_coor_2 = coor(2:2147,:);
+   %  IEN_2 = ien(8:2055,2:5);
+   %  % 关闭文件
+   %  fclose(fid);
 %% Read nodes and IEN for case 3
-    % 打开文件
-    fid = fopen('quarter-plate-with-hole-quad-Refine5.msh', 'r');
+   %  % 打开文件
+   %  fid = fopen('quarter-plate-with-hole-quad-Refine5.msh', 'r');
+   % 
+   %  if fid == -1
+   %      error('无法打开文件');
+   %  end
+   % 
+   %  % 初始化
+   %  i = 1;
+   %  j = 1;
+   %  coor = zeros(1,3);
+   %  ien  = zeros(1,5);
+   %  % 读取文件内容
+   % while ~feof(fid)
+   %      line = fgetl(fid);
+   %      num  = strsplit(line);
+   %      num = str2double(num(1,:));
+   %      Y = isnan(num);
+   %      num = num (~Y);
+   %      isdata = all(isnan(num));
+   %      if isdata == 0
+   %          if length(num) == 3
+   %              coor(i,:) = num;
+   %              i = i+1;
+   %          end
+   %          if length(num) == 5
+   %              ien(j,:) = num;
+   %              j = j+1;
+   %          end
+   %      end
+   % end
+   %  node_coor_3 = coor(2:8387,:);
+   %  IEN_3 = ien(8:8199,2:5);
+   %  % 关闭文件
+   %  fclose(fid);
 
-    if fid == -1
-        error('无法打开文件');
-    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    % 初始化
-    i = 1;
-    j = 1;
-    coor = zeros(1,3);
-    ien  = zeros(1,5);
-    % 读取文件内容
-   while ~feof(fid)
-        line = fgetl(fid);
-        num  = strsplit(line);
-        num = str2double(num(1,:));
-        Y = isnan(num);
-        num = num (~Y);
-        isdata = all(isnan(num));
-        if isdata == 0
-            if length(num) == 3
-                coor(i,:) = num;
-                i = i+1;
-            end
-            if length(num) == 5
-                ien(j,:) = num;
-                j = j+1;
-            end
-        end
-   end
-    node_coor_3 = coor(2:8387,:);
-    IEN_3 = ien(8:8199,2:5);
-    % 关闭文件
-    fclose(fid);
-
-
+    
 
 
 
